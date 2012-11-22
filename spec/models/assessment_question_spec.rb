cdc4e4cc30ee617f99219f2c11743ccc9a1d11d9
sam <sam@jiaoxuebang.com>
2012-11-22 14:05:11 +0800

    Remove unstable regular syntax

--- spec/models/assessment_question_spec.rb
+++ spec/models/assessment_question_spec.rb
@@ -212,5 +212,12 @@ describe AssessmentQuestion do
     html2 = "<p style=\"background: url(https://192.168.1.101:3000/5254/5566459337_abc140b4cb.jpg) no-repeat;\">foo</p>"
     AssessmentQuestion.sanitize(html2).should == html2
   end
+
+  it "should allow those style attributes" do
+    expected = "background: url( http://192.168.0.189:3000/courses/1/files/7/preview ) no-repeat scroll 0 0 transparent;max-height: 165px;max-width: 211px;min-height: 165px;min-width: 211px;"
+    AssessmentQuestion.sanitize(expected).should == expected
+    expected1 = "background:url( http://192.168.0.189:3000/files/7/download?verifier=BZEwpP2twf1QbpWOQvcTCQw6EtvhpSx0atsBZzlR ) no-repeat;max-width:165px;min-width:165px;max-height:165px;min-height:165px;"
+    AssessmentQuestion.sanitize(expected1).should == expected1
+  end
   
 end
