4028ab3feba5bf7034295c7ec789d0171cc7faa2
Ryan Wang <wongyouth@gmail.com>
2012-11-28 23:02:12 +0800

    fix for postgresql subdomains id started from 10000

--- db/migrate/20121127074037_jxb_make_subdomain_id_starts_from_10000.rb
+++ db/migrate/20121127074037_jxb_make_subdomain_id_starts_from_10000.rb
@@ -2,15 +2,12 @@ class JxbMakeSubdomainIdStartsFrom10000 < ActiveRecord::Migration
   tag :predeploy
 
   def self.up
-    # make next id started from 10000
-    execute <<-SQL
-      INSERT INTO SUBDOMAINS (id, account_id, subdomain) VALUES (9999, 1, 'ed9999');
-    SQL
-
-    # delete the row created above
-    execute <<-SQL
-      DELETE FROM SUBDOMAINS WHERE id = 9999;
-    SQL
+    case ActiveRecord::Base.configurations[RAILS_ENV]['adapter']
+    when 'postgresql'
+      execute "SELECT setval('subdomains_id_seq', 10000)"
+    when 'mysql', 'mysql2'
+      execute "ALTER TABLE subdomains INCREMENT = 10000"
+    end
   end
 
   def self.down
