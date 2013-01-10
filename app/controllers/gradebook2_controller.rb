4deb4beff1b0160b8f238b9e23006b2a1055bc3b
Ryan Wang <wongyouth@gmail.com>
2013-01-10 12:32:43 +0800

    bug fix: I18n cannot translate for breadcrumb

--- app/controllers/gradebook2_controller.rb
+++ app/controllers/gradebook2_controller.rb
@@ -1,6 +1,6 @@
 class Gradebook2Controller < ApplicationController
   before_filter :require_context
-  add_crumb( I18n.t('jxb.gradebook', "Gradebook") ) { |c| c.send :named_context_url, c.instance_variable_get("@context"), :context_grades_url }
+  add_crumb( lambda { I18n.t('jxb.gradebook', "Gradebook") } ) { |c| c.send :named_context_url, c.instance_variable_get("@context"), :context_grades_url }
   before_filter { |c| c.active_tab = "grades" }
 
   def show
