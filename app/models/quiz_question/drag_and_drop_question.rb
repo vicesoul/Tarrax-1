f65533f069fca70d26061e3c410e96068d243f78
Ryan Wang <wongyouth@gmail.com>
2013-03-01 16:17:44 +0800

    show incorrect answer for question type: drag_and_drop_question

--- app/models/quiz_question/drag_and_drop_question.rb
+++ app/models/quiz_question/drag_and_drop_question.rb
@@ -1,2 +1,9 @@
-class QuizQuestion::DragAndDropQuestion < QuizQuestion::MultipleDropdownsQuestion
+class QuizQuestion::DragAndDropQuestion < QuizQuestion::FillInMultipleBlanksQuestion
+  def find_chosen_answer(variable, response)
+    @question_data[:answers].detect{ |answer| answer[:id] == response.to_i } || { :text => nil, :id => nil, :weight => 0 }
+  end
+
+  def answer_text(answer)
+    answer[:id]
+  end
 end
