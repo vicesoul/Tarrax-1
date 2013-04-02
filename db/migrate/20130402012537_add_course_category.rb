bdae7e498c2653174d4f2ea37645fbe0c2107d59
tim <tim@jiaoxuebang.com>
2013-04-02 15:01:17 +0800

    Add course_category migrations

---
+++ db/migrate/20130402012537_add_course_category.rb
@@ -0,0 +1,10 @@
+class AddCourseCategory < ActiveRecord::Migration
+    tag :predeploy
+
+    def self.up
+        add_column :courses, :course_category_id, :integer, :limit => 8
+    end
+
+    def self.down
+    end
+end
