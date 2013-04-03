5c552caf6f99d34b2e9d0a095b123fa934642b8f
rupert <conveyr@qq.com>
2013-04-03 17:02:29 +0800

    new pages for pravicy and front-page

--- vendor/plugins/crummy/lib/crummy.rb
+++ vendor/plugins/crummy/lib/crummy.rb
@@ -81,7 +81,7 @@ module Crummy
     # Render the list of crumbs 
     def render_crumbs(options = {})
       if crumbs.length > 1
-        content_tag(:nav, :id => "breadcrumbs", :role => "navigation", 'aria-label' => 'breadcrumbs') do
+        content_tag(:nav, :id => "breadcrumbs", :role => "navigation", 'aria-label' => 'breadcrumbs', 'class' => 'blue-bar') do
           content_tag(:ul, nil, nil, false) do 
             crumbs.collect do |crumb|
               content_tag(:li, crumb_to_html(crumb), crumb[2])
