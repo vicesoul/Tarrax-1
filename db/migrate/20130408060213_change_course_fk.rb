cdc4d8bf4db1541664aca450ae6c903cf47f8ec6
tim <tim@jiaoxuebang.com>
2013-04-09 09:59:34 +0800

    remove database fk restrict

--- db/migrate/20130408060213_change_course_fk.rb
+++ db/migrate/20130408060213_change_course_fk.rb
@@ -2,7 +2,7 @@ class ChangeCourseFk < ActiveRecord::Migration
     tag :predeploy
 
     def self.up
-        change_column :courses, :course_category_id, :integer, :null => false
+        change_column :courses, :course_category_id, :integer
     end
 
     def self.down
