574b98e9e33c251e531046bf68a397fcca68ada4
Ryan Wang <wongyouth@gmail.com>
2013-04-11 09:37:45 +0800

    refs #526, fix spec for hiding course menu OUTCOME

--- lib/host_url.rb
+++ lib/host_url.rb
@@ -110,7 +110,7 @@ class HostUrl
     end
 
     def outgoing_email_default_name
-      @outgoing_email_default_name.presence || I18n.t("#email.default_from_name", "Instructure Canvas")
+      @outgoing_email_default_name.presence || I18n.t("#email.default_from_name", "Jiaoxuebang")
     end
 
     def file_host=(val)
