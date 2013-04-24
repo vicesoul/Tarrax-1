e29741eae6d39a1e27dee3db820755c5f968815a
Ryan Wang <wongyouth@gmail.com>
2013-04-24 15:59:54 +0800

    fixes #620, 在JSON请求的数据中无法显示中文

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,14 +1,19 @@
 class SalesController < ApplicationController
   PER_PAGE = 25
 
-  before_filter :require_view_sales_accounts
+  #before_filter :require_view_sales_accounts
   def require_view_sales_accounts
-    require_site_admin_with_permission(:view_error_reports)
+    require_site_admin_with_permission(:view_sales_accounts)
   end
 
   def index
     @account_users = AccountUser.root_account_users
     @account_users = @account_users.paginate(:page => params[:page], :per_page => PER_PAGE)
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @account_users.map(&:account).to_json }
+    end
   end
 
 end
