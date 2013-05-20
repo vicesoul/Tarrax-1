a653bfd39ace28b682edaab863f2f34ab923b29d
TimLang <langyong135@gmail.com>
2013-05-20 15:30:57 +0800

    job_position_category CRUD

---
+++ app/controllers/job_position_category_controller.rb
@@ -0,0 +1,36 @@
+class JobPositionCategoryController < ApplicationController
+
+  def new
+    @category = JobPositionCategory.new
+  end
+
+  def create
+    @category = JobPositionCategory.new(params[:job_position_category])
+    respond_to do |format|
+      if @category.save
+        format.html {redirect_to account_job_position_categories_url, :notice => 'Job position category was created successfully' }
+      else
+        format.html {render :action => 'new'}
+      end
+    end
+  end
+
+  def edit
+    @category = JobPositionCategory.find(params[:id])
+  end
+
+  def update
+    @category = JobPositionCategory.find(params[:id])
+    respond_to do |format|
+      if @category.update_attributes(params[:job_position_category])
+        format.html {redirect_to account_job_position_categories_url, :notice => 'Job position category was updated successfully' }
+      end
+    end
+  end
+
+
+  def index
+    @categories = JobPositionCategory.find_all_by_account_id(params[:account_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -442,6 +442,8 @@ ActionController::Routing::Routes.draw do |map|
 
     account.resources :grading_standards, :only => %w(index create update destroy)
 
+    account.resources :job_position_categories, :except => ['show'], :controller => 'job_position_category'
+
     account.statistics 'statistics', :controller => 'accounts', :action => 'statistics'
     account.statistics_graph 'statistics/over_time/:attribute', :controller => 'accounts', :action => 'statistics_graph'
     account.formatted_statistics_graph 'statistics/over_time/:attribute.:format', :controller => 'accounts', :action => 'statistics_graph'
