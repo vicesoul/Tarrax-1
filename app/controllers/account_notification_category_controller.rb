8aade98cf58abdc506d31690a952f59ad6e27c62
TimLang <langyong135@gmail.com>
2013-05-31 16:18:59 +0800

    fixed account notifition category auth bugs

--- app/controllers/account_notification_category_controller.rb
+++ app/controllers/account_notification_category_controller.rb
@@ -1,37 +1,35 @@
 class AccountNotificationCategoryController < ApplicationController
 
   before_filter :get_context
+  before_filter :auth
 
   def create
-    if authorized_action(@account, @current_user, nil, :manage_account_notification_category)
-      result = AccountNotificationCategory.create(
-        :name => params[:account_notification_category][:name],
-        :account_id => params[:account_notification_category][:account_id]
-      )
-      render :json => {:flag => !result.nil?, :categories => result }.to_json  
-    end
+    result = AccountNotificationCategory.create(
+      :name => params[:account_notification_category][:name],
+      :account_id => params[:account_notification_category][:account_id]
+    )
+    render :json => {:flag => !result.nil?, :categories => result }.to_json
   end
 
   def edit
   end
 
   def update
-    if authorized_action(@account, @current_user, nil, :manage_account_notification_category)
-      category = AccountNotificationCategory.find(params[:id])
-      category.name = params[:account_notification_category][:name]
-      result = category.save
-      render :json => {:flag => result, :categories => category, :type => 'edit' }.to_json  
-    end
+    category = AccountNotificationCategory.find(params[:id])
+    category.name = params[:account_notification_category][:name]
+    result = category.save
+    render :json => {:flag => result, :categories => category, :type => 'edit' }.to_json
   end
 
   def index
-    if authorized_action(@account, @current_user, nil, :manage_account_notification_category)
-      @show_left_side = true
-      @categories = AccountNotificationCategory.find_all_by_account_id(params[:account_id])
-    end
+    @show_left_side = true
+    @categories = AccountNotificationCategory.find_all_by_account_id(params[:account_id])
   end
 
-  def show
-  end
 
+  private
+
+  def auth
+    authorized_action(@context, @current_user, :manage_account_notification_category)
+  end
 end
